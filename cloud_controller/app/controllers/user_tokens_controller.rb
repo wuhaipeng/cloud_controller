26dba283902afb7d6841a6e0c648e30057fae775
Haipeng Wu <hwu@rbcon.com>
2013-02-28 22:50:20 -0700

    support ldap login

--- cloud_controller/app/controllers/user_tokens_controller.rb
+++ cloud_controller/app/controllers/user_tokens_controller.rb
@@ -1,9 +1,32 @@
+require 'net/ldap' 
+
 class UserTokensController < ApplicationController
 
   def create
     email = params['email']
     CloudController.logger.debug("Login request from #{email}")
     password = body_params[:password]
+    token = ldap_enabled? ? process_ldap_login(email, password) : process_local_login(email, password)
+    CloudController.logger.debug("Login request from #{email} token #{token.inspect}")
+    render :json => token
+  end
+
+  private
+  def process_ldap_login(email, password)
+    unless ldap_authenticate(email, password)
+      CloudController.logger.error("Authentication failed on LDAP server for #{email}")
+      raise CloudError.new(CloudError::FORBIDDEN)
+    end
+    user = ::User.find_by_email(email)
+    user = ::User.create_user(email, password) unless user
+    if AppConfig[:https_required] or (user.admin? and AppConfig[:https_required_for_admins])
+      CloudController.logger.error("Authentication failure: #{auth_token_header.inspect}", :tags => [:auth_failure])
+      raise CloudError.new(CloudError::HTTPS_REQUIRED) unless request_https?
+    end
+    UserToken.create(email)
+  end
+
+  def process_local_login(email, password)
     if ::User.valid_login?(email, password) || (@current_user && @current_user.admin?)
       # This could just check the ::User.admins variable, but using this method to support changes in admin? in the future
       user = ::User.find_by_email(email)
@@ -33,10 +56,25 @@ class UserTokensController < ApplicationController
       if token.nil?
         token = UserToken.create(email)
       end
-      CloudController.logger.debug("Login request from #{email} token #{token.inspect}")
-      render :json => token
+      token
     else
       raise CloudError.new(CloudError::FORBIDDEN)
     end
+  end
+
+  def ldap_authenticate(email,password)
+    user = email.gsub(%r(@.*), "")
+    auth = "uid=#{user}"
+    email.gsub(%r(.*@), "").split('.').each { |s| auth += ",dc=#{s}" }
+
+    ldap = Net::LDAP.new
+    ldap.host = AppConfig[:ldap][:host]
+    ldap.port = AppConfig[:ldap][:port]
+    ldap.auth auth, password
+    ldap.bind
+  end
+
+  def ldap_enabled?
+    AppConfig[:ldap][:enabled]
   end
 end
